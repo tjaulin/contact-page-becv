480a5a0bc182df1fb50fb4f3117f783f71d0e962
Thomas <thomas.jaulin@mcparq.be>
2023-10-11 17:01:40 +0200

    js progress

--- js/script.js
+++ js/script.js
@@ -11,8 +11,43 @@ btnSubmit.addEventListener('click', (e) => {
 
     e.preventDefault();
 
-    // content.innerHTML = ` `;
-
+    content.innerHTML = `
+    <div class="d-flex justify-content-center flex-column align-items-center">
+        <i class="valid-icon fa-solid fa-circle-check fa-7x"></i>
+        <h1 class="text-center mb-3">Your message was successfully submitted !</h1>
+    </div>
+    <div class="w-100">
+        <table class="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th class="text-center" colspan="5">Resume table</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>${name.value}</td>
+                </tr>
+                <tr>
+                    <td>Company Email</td>
+                    <td>${companyEmail.value}</td>
+                </tr>
+                <tr>
+                    <td>Company Size</td>
+                    <td>${companySize.textContent}</td>
+                </tr>
+                <tr>
+                    <td>Subject</td>
+                    <td>${subject.textContent}</td>
+                </tr>
+                <tr>
+                    <td>Message</td>
+                    <td>${message.value}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `;
     console.log(name.value, companyEmail.value, companySize.textContent, subject.textContent, message.value);
 });
 
